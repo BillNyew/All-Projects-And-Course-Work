3b6bf98545c27af228636fca129176df7b364463
Bill Turbadrakh <bill9turbo@yahoo.com>
2025-12-19 03:03:33 -0700

    Updates

--- 1st Year Python/lab21.py
+++ 1st Year Python/lab21.py
@@ -1,12 +1,1 @@
-def modified_selection_sort(numbers):
-    n = len(numbers)
-    for i in range(n-1, 0, -1):
-        max_index = 0
-        for j in range(1, i+1):
-            if numbers[j] > numbers[max_index]:
-                max_index = j
-        numbers[max_index], numbers[i] = numbers[i], numbers[max_index]
-        print(numbers)
-if __name__ == "__main__":
-    numbers = [int(number) for number in input().split()]
-    modified_selection_sort(numbers)
+
--- 1st Year Python/lab27.py
+++ 1st Year Python/lab27.py
@@ -1,14 +1,37 @@
-import numpy as np
-from random import randint, seed
+from random import randint
+outputs = ["It is certain.",
+"It is decidedly so.",
+"Without a doubt.",
+"Yes definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."]
 
-seed(13)
-numbers = np.zeros(100, dtype=int)
 
-for i in range(10):
-    numbers[i+1] = randint(1, 100)
+question = input("Im the magic 8 ball, ask me a yes/no question: ")
 
-for i in range(10, 0, -1):
-    numbers[i] = numbers[i-1]
-numbers[0] = 0
-print(numbers)
 
+print(f'{outputs[randint(0,len(outputs))]}')
+
+done = False
+while not done:
+    question = question.lower()
+    if question == 'done':
+        done = True
+        print("Have a good day!")
+    elif question != 'done':
+        question = input("What is your next quesiton?: ")
+        print(f'{outputs[randint(0,len(outputs))]}')
